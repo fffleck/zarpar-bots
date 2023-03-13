8ef0ade2cd9ba4d1078364d148beeaa803322c47
AuroraDark <andressa.workti@gmail.com>
2023-03-13 20:11:22 -0300

    Fixing deployment issue

--- armadores/zim/zimBot.js
+++ armadores/zim/zimBot.js
@@ -20,6 +20,7 @@ const zimbot = async (
     const browser = await puppeteer.launch({
       headless: true,
       executablePath: executablePath(),
+
       // executablePath: "/usr/bin/google-chrome",
       ignoreHTTPSErrors: true,
       ignoreDefaultArgs: ["--enable-automation"],
