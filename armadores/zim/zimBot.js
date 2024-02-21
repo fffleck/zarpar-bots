77937b812d60f0ae9781f1181a1d8ce0cd257569
Fabio Fleck <ffleck@gmail.com>
2024-02-21 20:39:10 -0300

    Atualizando

--- armadores/zim/zimBot.js
+++ armadores/zim/zimBot.js
@@ -17,7 +17,6 @@ const zimbot = async (
 ) => {
   try {
     // Criando o browser
-    console.log("Chegou aqui");
     const browser = await puppeteer.launch({
       headless: true,
       executablePath: executablePath(),
